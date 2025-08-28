75d9f5d6a5b134394465c8b7e10a6f94c9fc21fe
MateusMNogueira <mateusmn04@gmail.com>
2025-08-28 12:05:27 -0300

    codigoquestoes

---
+++ questao1.c
@@ -0,0 +1,6 @@
+int main(){
+    int x;
+    x = 10;
+    printf("O valor de x eh: %d\n", x);
+    return 0;
+}
---
+++ questao2.c
@@ -0,0 +1,11 @@
+int main(){
+    int x,y;
+    int soma=0;
+    scanf("%d", &x);
+    scanf("%d", &y);
+    soma = x + y;
+    printf("A soma de x e y eh: %d\n", soma);
+    return 0;
+
+
+} 
